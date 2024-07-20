d8b29947a062cd4c6d2d30d16fc7dc37832c3672
Elvis <elvis.omondi@student.moringaschool.com>
2024-07-21 00:01:45 +0300

    commit

--- index.js
+++ index.js
@@ -1,37 +1,20 @@
-/*const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+const tutorials = [
+  "what does the this keyword mean?",
+  "what is the constructor oo pattern?",
+  "implementing blockchain web api",
+  "the test driven development workflow",
+  "what is nan and how can we check for it",
+  "what is the difference between stoppropagation and preventdefault?",
+  "immutable state and pure functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is jsonp?"
 ];
 
-const titleCased = () => {
-  return tutorials.map();
-}
-
-console.log(typeof tutorials)*/
-
-function titleCased(tutorials) {
-  if (!Array.isArray(tutorials)) {
-    throw new Error('Expected an array of tutorials');
-  }
-
+function titleCased() {
   return tutorials.map((tutorial) => {
-    if (typeof tutorial!== 'tring') {
-      throw new Error('Expected a string tutorial name');
-    }
-
-    const words = tutorial.split(' ');
-    const titleCasedWords = words.map((word) => {
-      return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-    });
-
-    return titleCasedWords.join(' ');
+    return tutorial.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
   });
 }
+
+module.exports = titleCased;
